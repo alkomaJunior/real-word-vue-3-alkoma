bbfb5af9e4a4c6c6c51b841be13afc966706423a
Aimé ALOGNIKOU <48490387+alkomaJunior@users.noreply.github.com>
2021-12-19 00:12:43 +0100

    Introduction to dynamic routing.

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <EventDetailsLayout :event="event"></EventDetailsLayout>
+</template>
+
+<script>
+import EventDetailsLayout from '@/components/EventDetailsLayout'
+import EventService from '@/services/EventService'
+
+export default {
+  name: 'EventDetails',
+  components: {
+    EventDetailsLayout,
+  },
+  props: ['id'],
+  data() {
+    return {
+      event: null,
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+}
+</script>
+
+<style scoped></style>
